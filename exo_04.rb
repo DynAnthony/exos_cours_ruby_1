93c5d299f7b3f67b4ad95545abd3d3b6a79b25d1
BSarahB <bslajta@gmail.com>
2019-10-01 16:39:37 +0200

    Adding correction

---
+++ exo_04.rb
@@ -0,0 +1,3 @@
+puts "Salut, ca farte ?
+
+#Le message ne pourra etre lu car l erreur retournee est une ereur de synthaxe, en effet, Salut ca farte? est une variable de type string , cest a dire de chaines de caracteres soit une phrase. ce type de variable doit etre contenu entre des guillemets "" il manque donc un guillement a la fin de la phrase.
