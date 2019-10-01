872f46c32f5cb3e0dcf98e42e5668fefd295e3aa
BSarahB <bslajta@gmail.com>
2019-10-01 17:13:10 +0200

    adding commentary

---
+++ exo_06.rb
@@ -0,0 +1,7 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+#On declare trois variables qui prennent comme valeur chacune un chiffre la var 1 prend la valeur 10, la deuxieme 5 la 3e 11. dans la concactenation #{} on fait le produit des 3 variables qui donnen le resultat 550 et on peut lier ce resultat dans notre string Travail : 550
