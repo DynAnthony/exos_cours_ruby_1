a37a21271147d5ec9557f4339e834be2038e1b78
BSarahB <bslajta@gmail.com>
2019-10-02 00:00:47 +0200

    exo07c

--- exo_07_c.rb
+++ exo_07_c.rb
@@ -1,2 +1,7 @@
 user_name = gets.chomp
 puts user_name
+
+#la difference entre les trois programmes est que la variable puts permet une remise a la ligne des infos 
+#la commande print affiche a la console le contenu sans remettre a la ligne les elements. 
+#la question pour demander le nom manque dans le 7c
+#le signe > peut etre une invitation a ecrire ou tapper un texte
